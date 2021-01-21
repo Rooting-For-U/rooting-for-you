d2e07b0955608acdb3c5be133f404fd10389888b
Ying Guo <guoguoying2013@gmail.com>
2021-01-21 14:48:10 -0800

    Pair programming with Marissa on Homepage, Plant and CSS sytling.

--- client/src/components/Plant.jsx
+++ client/src/components/Plant.jsx
@@ -1,8 +1,36 @@
 import React, { useState } from 'react';
 
 // send plantId, timestamp, param to server when water something
-const Plant = () => (
-  <div>Plant detail page</div>
-);
+const Plant = (plant) => {
+  console.log('Single Plant is called');
+  console.log('Plant input: ', plant);
+
+  const [waterDate, setWaterDate] = useState('');
+  const now = new Date();
+  const today = now.getDate();
+  const lastWater = new Date(plant.plant.waterDateTime);
+  const lastWaterDate = lastWater.getDate();
+  const daysNoWater = today - lastWaterDate;
+  console.log('daysNoWater', daysNoWater);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    alert('Clicked water button');
+  };
+
+  return (
+    <div className="single-plant-box" key={plant.plant.plantName}>
+      <img src={plant.plant.imgURL} alt="plant" width="100" height="120" />
+      <span>{plant.plant.plantName}</span>
+      <br />
+      <span>Last time watered: </span>
+      <span>{plant.plant.waterDateTime.slice(0, 16)}</span>
+      <br />
+      <span>{plant.plant.location}</span>
+      <br />
+      <button type="button" onClick={handleClick}>Water</button>
+    </div>
+  );
+};
 
 export default Plant;
