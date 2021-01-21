eed3c0f08ae4ef4b946df734a4c4d95bc6a4f7bf
Ying Guo <guoguoying2013@gmail.com>
2021-01-21 13:22:52 -0800

    Homepage and Plant component

---
+++ client/src/components/Plant.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+// send plantId, timestamp, param to server when water something
+const Plant = () => (
+  <div>Plant detail page</div>
+);
+
+export default Plant;
